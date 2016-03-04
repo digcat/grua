ee2c4932a38b4cf5f072f370b152f90cd764b0af
Martin Cosgrave <martin@bettercode.com>
2016-03-03 21:43:41 -0500

    .

--- grua/util.py
+++ grua/util.py
@@ -19,16 +19,46 @@ def note(msg, ignore_quiet=False):
         print "> " + msg
 
 def find_bridge_ip():
-    command = ["ip", "addr", "show", "dev", "docker0"]
-    sp = subprocess.Popen((command), stdout=subprocess.PIPE)
+    done = False
+    try:
+        command = ["ip", "addr", "show", "dev", "docker0"]
+        sp = subprocess.Popen(command, stdout=subprocess.PIPE)
 
-    output = subprocess.check_output(('grep', 'inet'), stdin=sp.stdout).strip().split()[1].split('/')[0]
+        output = subprocess.check_output(('grep', 'inet'), stdin=sp.stdout).strip().split()[1].split('/')[0]
+
+        done = True
+
+    except OSError as e:
+        if e.errno == os.errno.ENOENT:
+            # handle file not found error.
+            done = False
+        else:
+            # Something else went wrong
+            raise
+
+    if not done:
+        try:
+            command = ["ifconfig", "docker0"]
+
+            sp = subprocess.Popen(command, stdout=subprocess.PIPE)
+
+            output = subprocess.check_output(('grep', 'inet'), stdin=sp.stdout).strip().split(':')[1].split()[0]
+
+        except OSError as e:
+            if e.errno == os.errno.ENOENT:
+                # handle file not found error.
+                done = False
+            else:
+                raise
+
+    if not done:
+        raise Exception("Could not find either 'ip' or 'ifconfig' in PATH")
 
     sp.wait()
 
     # ensure we have a valid ip
     p = re.compile(
-        '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$')
+            '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$')
     if not p.match(output):
         raise Exception(output + " is not a valid IP address for BridgeIP")
 
